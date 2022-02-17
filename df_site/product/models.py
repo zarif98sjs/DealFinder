4d2fc5b6fc92d79ee030130dd40ffebc73a4167e
ramisa2108 <1705004@ugrad.cse.buet.ac.bd>
2022-02-17 13:28:53 +0600

    database instert added

--- df_site/product/models.py
+++ df_site/product/models.py
@@ -1,3 +1,110 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+class Website(models.Model):
+	"""Model representing a website"""
+	# primary key
+	website_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for website')
+
+	website_name = models.CharField(max_length=200)
+	website_url = models.URLField(max_length=200)
+
+	def __str__(self):
+		return self.website_name
+
+
+class Product(models.Model):
+	"""Model representing a product"""
+	# primary key
+	product_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for product')
+
+	product_name = models.CharField(max_length=200)
+	product_category = models.CharField(max_length=200)
+	product_subcategory = models.CharField(max_length=200)
+	product_brand = models.CharField(max_length=200)
+	product_buy_count = models.IntegerField(default=0)
+
+	class Meta:
+		ordering = ['product_name', 'product_category', 'product_subcategory', 'product_brand']
+
+	def __str__(self):
+		"""String for representing the Model object."""
+		return self.product_name
+
+
+class ProductWebsite(models.Model):
+	"""Model representing product-website relation"""
+
+	# primary key
+	product_website_id = models.UUIDField(
+		primary_key=True, default=uuid.uuid4, help_text='Unique ID for product-website relation')
+
+	# foreign keys
+	product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, unique=False)
+	website = models.ForeignKey(Website, on_delete=models.CASCADE, null=True, unique=False)
+
+	shipping_time = models.DateTimeField(null=True, blank=True)
+	delivery_charge = models.DecimalField(default=0, max_digits=6, decimal_places=2)
+	popularity_count = models.IntegerField(default=0)
+	price = models.DecimalField(max_digits=10, decimal_places=2)
+	available_count = models.IntegerField(default=0)
+	image_path = models.CharField(null=True, max_length=200)
+
+	class Meta:
+		ordering = ['price']
+
+	def __str__(self):
+		return self.product.product_name + ", " + self.website.website_name
+
+
+class Offer(models.Model):
+	"""Model representing offers"""
+	# primary key
+	offer_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for offer')
+
+	# foreign key
+	product_website = models.ForeignKey(ProductWebsite, on_delete=models.CASCADE, null=True)
+
+	discount_percentage = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+	discount_amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+	start_date = models.DateTimeField(null=True, blank=True)
+	end_date = models.DateTimeField(null=True, blank=True)
+	is_free_shipping = models.BooleanField(default=False)
+	buy_one_get_one_free = models.BooleanField(default=False)
+
+	class Meta:
+		ordering = ['discount_percentage']
+
+	def __str__(self):
+		return self.offer_id
+
+
+class Specification(models.Model):
+	"""Model representing specifications"""
+	# primary key
+	spec_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for specification')
+	spec_name = models.CharField(max_length=1000)
+
+	class Meta:
+		ordering = ['spec_name']
+
+	def __str__(self):
+		return self.spec_name
+
+
+class ProductSpecification(models.Model):
+	"""Model representing relation between product and specifications"""
+	# primary key
+	product_spec_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for product specification relation')
+	# foreign keys
+	product_website = models.ForeignKey(ProductWebsite, unique=False, on_delete=models.CASCADE)
+	spec = models.ForeignKey(Specification, unique=False, on_delete=models.CASCADE)
+	spec_val = models.CharField(default="", max_length=1000)
+	# class Meta:
+	# 	unique_together = ('product_website', 'spec')
+
+	def __str__(self):
+		return self.product_website.product.product_name + ": " + self.spec.spec_name
--- df_site/product/views.py
+++ df_site/product/views.py
@@ -1,105 +1,153 @@
 from django.shortcuts import render
+import json
+import re
+from decimal import Decimal
+from product.models import Website, Product, ProductWebsite, Offer, Specification, ProductSpecification
 
 # Create your views here.
+loaded = False
+
+
+def load_database():
+	global loaded
+	if loaded:
+		return
+
+	Product.objects.all().delete()
+	ProductWebsite.objects.all().delete()
+	Specification.objects.all().delete()
+	ProductSpecification.objects.all().delete()
+	Offer.objects.all().delete()
+
+	websites = Website.objects.all()
+
+	for website in websites:
+		try:
+			with open("../scraper/" + website.website_name + ".json") as file:
+				products = json.load(file)['products']
+				for p in products:
+					regular_price = Decimal(re.sub("[^0-9.]", "", p['Regular Price']))
+					discount_amount = regular_price - Decimal(re.sub("[^0-9.]", "", p['Offer Price']))
+
+					product = Product(
+						product_name=p['Title'], product_category=p['Category'],
+						product_subcategory=p['Sub Category'], product_brand=p['Brand']
+					)
+					product.save()
+
+					product_website = ProductWebsite(
+						product=product, website=website, image_path=p['Image URL'],
+						price=regular_price
+					)
+					product_website.save()
+
+					offer = Offer(
+						product_website=product_website, discount_amount=discount_amount,
+						discount_percentage=(discount_amount / regular_price) * 100
+					)
+					offer.save()
+
+				# for s in p['Specs']:
+				#     specification = Specification(spec_name=s)
+				#     specification.save()
+				#     print("spec saved")
+				#     product_specification = ProductSpecification(
+				#         product_website=product_website, specification=specification
+				#     )
+				#     product_specification.save()
+				#     print("prod spec saved")
+
+		except Exception as e:
+			print("Problem in ", website)
+			print(e)
+			pass
+	loaded = True
+
 
 def home(request):
-    # homepage load
-    #---------------load category names in categories as list--------------------
-    categories = ['category1', 'category2', 'category3', 'category4', 'category5','category6','category7']
-    #also need to send the top products, trending deals etc 
+	load_database()
+	# homepage load
+	# ---------------load category names in categories as list--------------------
+	categories = ['category1', 'category2', 'category3', 'category4', 'category5']
+	# also need to send the top products,  trending deals etc
+	trending_deals = ['product1', 'product2', 'product3', 'product4', 'product5', 'product6']
+	editors_pick = ['product1', 'product2', 'product3', 'product4', 'product5', 'product6']
+	featured_products = ['product1', 'product2', 'product3', 'product4', 'product5', 'product6']
+	top_offers = ['product1', 'product2', 'product3', 'product4', 'product5', 'product6']
+	return render(request, 'product/index.html', {
+		'categories': categories, 'trending_deals': trending_deals, 'editors_pick': editors_pick,
+		'featured_products': featured_products, 'top_offers': top_offers
+	})
 
-    #---------------load trending_deals,editors_pick,featured_products names,top_offers in corresponding  lists to show at homepage--------------------
-    trending_deals=['product1','product2','product3','product4','product5','product6']
-    editors_pick=['product1','product2','product3','product4','product5','product6']
-    featured_products=['product1','product2','product3','product4','product5','product6']
-    top_offers=['product1','product2','product3','product4','product5','product6']
-    return render(request, 'product/index.html', {'categories' : categories,'trending_deals':trending_deals,'editors_pick':editors_pick,'featured_products':featured_products,'top_offers':top_offers} )
 
 def select_category(request, category_name):
-    print(category_name)
-    # get product_list according to category
-    title = "1stPlayer FK 300W Power Supply"
-    url = "https://www.startech.com.bd/toshiba-dt01aba100v-1tb-surveillance-hdd"
-    image_url = "https://www.startech.com.bd/image/cache/catalog/power-supply/1stplayer/fk-300w/fk-300w-01-228x228.jpg"
-    price = 2200
-    # specs = [Modular: Non-Modular, Input Frequency: 50-60Hz
-    #                 Input Current: Current 8/10A MAX
-    #                 Fan Size120MM Hydraulic Bearing
-    #                 Certifications: None 80+]
-    # The plan is to show the specs details when clicked in view details
-    specs = ""
-    brand = "Thermaltake"
-    product_list = [    
-                    [title, url, image_url, price, specs, brand],
-                    [title, url, image_url, price, specs, brand],
-                    [title, url, image_url, price, specs, brand],
-                    [title, url, image_url, price, specs, brand],
-                    [title, url, image_url, price, specs, brand],
-                ]
-    return render(request, 'product/shop.html', { 'search_key' : category_name, 'product_list' : product_list  } )
+	print(category_name)
+	# get product_list according to category
+	product_list = []
+	return render(request, 'product/shop.html', {'search_key': category_name, 'product_list': product_list})
 
 
 def search(request):
-    # after entering something in the search bar of home page
-    # pass Model product object list
-    # Reference : https://stackoverflow.com/questions/16829764/how-to-pass-an-object-to-html-in-django-view-py
-    # -------------------------------------Dummy Product List-----------------------------------------------
-    title = "1stPlayer FK 300W Power Supply"
-    url = "https://www.startech.com.bd/toshiba-dt01aba100v-1tb-surveillance-hdd"
-    image_url = "https://www.startech.com.bd/image/cache/catalog/power-supply/1stplayer/fk-300w/fk-300w-01-228x228.jpg"
-    price = 2200
-    # specs = [Modular: Non-Modular, Input Frequency: 50-60Hz
-    #                 Input Current: Current 8/10A MAX
-    #                 Fan Size120MM Hydraulic Bearing
-    #                 Certifications: None 80+]
-    # The plan is to show the specs details when clicked in view details
-    specs = ""
-    brand = "Thermaltake"
-    product_list = [    
-                    [title, url, image_url, price, specs, brand],
-                    [title, url, image_url, price, specs, brand],
-                    [title, url, image_url, price, specs, brand],
-                    [title, url, image_url, price, specs, brand],
-                    [title, url, image_url, price, specs, brand],
-                ]
-    # -------------------------------------Dummy Product List-----------------------------------------------
-    if request.method=='POST':
-        print(request.POST['search_key'])
-        # check if search_key is a brand name or product name
-        # if brand : make product list accordingly
-        # if product : make product list accordingly
-    
-    return render(request, 'product/shop.html', {'search_key' : request.POST['search_key'], 'product_list' : product_list})
+	# after entering something in the search bar of home page
+	# pass Model product object list
+	# Reference : https://stackoverflow.com/questions/16829764/how-to-pass-an-object-to-html-in-django-view-py
+	# -------------------------------------Dummy Product List-----------------------------------------------
+	title = "1stPlayer FK 300W Power Supply"
+	url = "https://www.startech.com.bd/toshiba-dt01aba100v-1tb-surveillance-hdd"
+	image_url = "https://www.startech.com.bd/image/cache/catalog/power-supply/1stplayer/fk-300w/fk-300w-01-228x228.jpg"
+	price = 2200
+	# specs = [Modular: Non-Modular,  Input Frequency: 50-60Hz
+	#                 Input Current: Current 8/10A MAX
+	#                 Fan Size120MM Hydraulic Bearing
+	#                 Certifications: None 80+]
+	# The plan is to show the specs details when clicked in view details
+	specs = ""
+	brand = "Thermaltake"
+	product_list = [
+		[title, url, image_url, price, specs, brand],
+		[title, url, image_url, price, specs, brand],
+		[title, url, image_url, price, specs, brand],
+		[title, url, image_url, price, specs, brand],
+		[title, url, image_url, price, specs, brand],
+	]
+	# -------------------------------------Dummy Product List-----------------------------------------------
+	if request.method == 'POST':
+		print(request.POST['search_key'])
+	# check if search_key is a brand name or product name
+	# if brand : make product list accordingly
+	# if product : make product list accordingly
+
+	return render(request, 'product/shop.html',{
+		'search_key': request.POST['search_key'], 'product_list': product_list
+	})
 
 
 def search_name(request, search_key):
-    # search inside a product / brand page
-    # print(product_list)
-    if request.method=='POST':
-        print(request.POST['search_name'])
-        # check if search_name is a brand name or product name
-        # if brand : make product list accordingly
-        # if product : make product list accordingly
+	# search inside a product / brand page
+	# print(product_list)
+	if request.method == 'POST':
+		print(request.POST['search_name'])
+	# check if search_name is a brand name or product name
+	# if brand : make product list accordingly
+	# if product : make product list accordingly
 
-    product_list = []
-    return render(request, 'product/shop.html', {'search_key' : search_key, 'product_list' : product_list})
+	product_list = []
+	return render(request, 'product/shop.html', {'search_key': search_key, 'product_list': product_list})
+
 
 def sort(request, search_key, sort_type):
-    print(sort_type)
-    # sort accordingly
-    # access the product list from session ??
-    # Reference : https://stackoverflow.com/questions/9024160/django-pass-object-from-view-to-next-for-processing
-    product_list = []
-    return render(request, 'product/shop.html', {'search_key' : search_key, 'product_list' : product_list})
-
-
-
+	print(sort_type)
+	# sort accordingly
+	# access the product list from session ??
+	# Reference : https://stackoverflow.com/questions/9024160/django-pass-object-from-view-to-next-for-processing
+	product_list = []
+	return render(request, 'product/shop.html', {'search_key': search_key, 'product_list': product_list})
 
 ###################################################################################################################
 # Naeem - To do list
-# Product details pop up - with specs 
-# Product details show from model object - Stack overflow reference in search function 
+# Product details pop up - with specs
+# Product details show from model object - Stack overflow reference in search function
 # Filter implementation - same as sort
-# Remove Unnecessary things from homepage and other pages 
+# Remove Unnecessary things from homepage and other pages
 # Follow Mock UI design
 ###################################################################################################################
